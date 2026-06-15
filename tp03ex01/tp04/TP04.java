4f6b722890bc1640b18fda11e99394f6cc866e4a
ElJonas <joaovsant26@gmail.com>
2026-06-15 18:09:13 -0300

    Add files via upload

---
+++ tp03ex01/tp04/TP04.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package tp03ex01.tp04;
+import java.util.*;
+/**
+ *
+ * @author PICHAU
+ */
+public class TP04 {
+
+    public static void main(String[] args) {
+        Data teste = new Data();
+            
+    }
+}
